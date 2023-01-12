8796c864dbda737393f61d1b7e5719c6d3542110
Christopher Dean Canlas <chrisdcanlas@gmail.com>
2023-01-11 19:38:52 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ft){
   // Write your code here!
+  if(ft <= 400) {
+    return 'This one is on me!';
+  }
+  else if(400 < ft && ft< 2000) {
+    return 'That will be twenty bucks.';
+  }
+  else if(2000 <= ft && ft <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city == 'NYC'? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+      break;
+  }
 }
